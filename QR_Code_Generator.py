7fade5d56b395d0fb9faecabd1183af2e603ac52
Julius-Okeyo <jaykopiyo@gmail.com>
2022-09-04 16:56:07 +0300

    Updated QR Code generator

--- QR_Code_Generator.py
+++ QR_Code_Generator.py
@@ -1,10 +1,8 @@
+''' Given some data the code generates a QR Code
+for the data.'''
+
 import pyqrcode
-#import sys
-from PIL import Image
 
 link = input('Link for which to generate a QR code: ')
 QRCode = pyqrcode.create(link)
-#QRCode.svg(sys.stdout,scale=1)
-#QRCode.svg('masenoschool.svg',scale=4)
-pyqrcode.QRCode.png(QRCode,scale=5)
-Image.open('QRCode.png')
+QRCode.svg('QR_Code.svg',scale=4)
